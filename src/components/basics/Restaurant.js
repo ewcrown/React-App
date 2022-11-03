817ced0a7377fe5a0f461f8a6971c0c1e6a55ba6
ewcrown <hrxkhan007@gmail.com>
2022-11-03 17:53:02 +0500

    initial commit

---
+++ src/components/basics/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'
+
+const Restaurant = () => {
+    return (
+        <section>
+            <h1>Restaurant</h1>
+        </section>
+    )
+}
+
+export default Restaurant
